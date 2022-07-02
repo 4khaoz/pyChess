d3832d2c11d177411851ed649be3951beda6302c
4khaoz <thaitung.nguyen@yahoo.de>
2022-07-02 12:44:28 +0200

    Initial Chess Piece setup

--- board.py
+++ board.py
@@ -13,9 +13,7 @@ class Board(IDrawable, object):
 
     def __init__(self):
         self.pos_data = self.generate_empty_board()
-
-        # Test
-        self.pos_data[0][4].piece = Piece("black_king.png")
+        self.__init_chess_pieces()
 
     def generate_empty_board(self):
         """
@@ -25,6 +23,19 @@ class Board(IDrawable, object):
             [Position(x, y, self.FIELD_SIZE, Color(self.FIELD_COLORS[(x+y)%2])) for x in range(Board.COLS)] 
             for y in range(Board.ROWS)
         ]
+
+    def __init_chess_pieces(self):
+        pieces = ["rook", "knight", "bishop", "queen", "king", "bishop", "knight", "rook"]
+        for x in range(self.COLS):
+            # Pawns
+            self.pos_data[1][x].piece = Piece("black_pawn.png")
+            self.pos_data[self.ROWS - 2][x].piece = Piece("white_pawn.png")
+
+            # Black
+            self.pos_data[0][x].piece = Piece("black_" + pieces[x] + ".png")
+
+            # White
+            self.pos_data[self.ROWS - 1][x].piece = Piece("white_" + pieces[x] + ".png")
 
     def update_position(self, x, y, piece: Piece):
         """
